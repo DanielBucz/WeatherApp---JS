ff39d7f822740f14044390c380d485d22d2da971
Daniel <buczkowskidaniel202@gmail.com>
2024-02-07 22:13:01 +0100

    Pierwsza funkcja pogodowa

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+const APL_LINK  = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = '&appid=b659bc9e7acb68dffcf8d9b44e17bc3b';
+const API_UNITS = '&units=metric';
+
+const getWeather = () => {
+    const city = input.value || 'Warszawa';
+    const URL = APL_LINK+city+API_KEY+API_UNITS;
+
+    axios.get(URL)
+    .then(res => {
+        const temp = res.data.main.temp;
+        const hum = res.data.main.humidity;
+
+        cityName.textContent = res.data.name;
+        temperature.textContent = Math.floor(temp) + '°C';
+        humidity.textContent = hum + '%';
+    })
+    .catch(err => {
+        console.error(err); 
+    })
+
+}
+getWeather()
